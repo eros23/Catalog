aa9fe1e975a0e67cfed19da9d5d5ee9b1a777b8f
vecchiojoe <vecchio_joe@hotmail.com>
2013-07-26 12:16:07 +0200

    start pageadd
    start page add e fix page list

--- admin/public/pageadd.php
+++ admin/public/pageadd.php
@@ -18,7 +18,30 @@ function catalog_pageadd($ok=false) {
     <a href="#" class="support unselectable">Documentazione</a>
     <a href="#" class="clear unselectable"></a>
   </div>
-    
+<form id="settingsForm" method="post">
+  <div class="row-fluid">
+    <div class="span12" style="text-align: center;">
+      <input class="btn" type="submit" value="Salva Modifiche" />
+    </div>
+  </div>
+
+  <div class="row-fluid">
+    <div class="span6">
+      <h3 class="heading">Configurazione</h3>            
+      <div class="formSep">
+        <label>Sito Offline?</label>
+        <select name="xoffline" class="span6">
+        <option value="0" selected>No</option>
+        <option value="1">Si</option>
+        </select>
+        <label>Messaggio sito offline:</label>
+        <textarea class="span6" rows="4" cols="1"  name="xoffline_message">Sito Off line! Si prega di tornare piÃ¹ tardi</textarea>
+      </div>
+    </div>
+    <div class="span6">
+      <h3 class="heading">SEO</h3>
+    </div>
+</form>    
         
          <?php
  include_once('footer.php');
--- admin/public/pagelist - Copia.php
+++ admin/public/pagelist - Copia.php
@@ -1,4 +1,4 @@
-<?php  // Last Updated: 25/07/2013
+<?php  // Last Updated: 22/07/2013
 
 	  if (!defined('ADMIN_FILE')) 
 	  	 { die( include('../../../error_file/alert.php') ); }
@@ -12,32 +12,33 @@ function catalog_pagelist() {
     	 $ofsbgn = ($page * $ofsppg) - $ofsppg;
          ?>
 <div class="ls-icon-layers"></div>
-  <h2><?php echo _CATALOG_PAGELIST; ?><span class="pull-right"><a href="#" class="add-new-h2"></a></span></h2>
+  <h2><?php echo ''._CATALOG_PAGELIST.''; ?><span class="pull-right"><a href="#" class="add-new-h2"></a></span></h2>
 
   <!-- Main menu bar -->
   <div id="ls-main-nav-bar">
-    <a href="<?php echo $admin_file.'.php?op='.$name_prefix.'_pageadd' ;?>" class="layers active">Aggiungi Prodotto</a>
-    <a href="<?php echo $admin_file.'.php?op='.$name_prefix.'_main&daynum=10' ;?>" class="callbacks">Torna elenco</a>
+    <a href="<?php echo ''.$admin_file.'.php?op='.$name_prefix.'_pageadd' ;?>" class="layers active">Aggiungi Prodotto</a>
+    <a href="<?php echo ''.$admin_file.'.php?op='.$name_prefix.'_main&daynum=10' ;?>" class="callbacks">Torna elenco</a>
     <a href="#" class="support unselectable">Documentazione</a>
     <a href="#" class="clear unselectable"></a>
   </div>
     
+        
 <div class="row-fluid">
   <div class="span12">
-    <table class="table table-bordered table-striped table_vam" id="dt_gal">
+    <table class="table table-striped table-bordered mediaTable">
       <thead>
       <tr>
-        <th>#</th>
-        <th style="width:60px"><?php echo _CATALOG_ICON; ?></th>
-        <th><?php echo _TITLE; ?></th>
-        <th><?php echo _WEIGHT; ?></th>
-        <th><?php echo _CATALOG_CODE; ?></th>
-        <th><?php echo _DATEINS; ?></th>
-        <th><?php echo _CATALOG_CATEGORY; ?></th>
-        <th><?php echo _LANGUAGE; ?></th>
-        <th><?php echo _STATUS; ?></th>
-        <th><?php echo _CATALOG_HITS; ?></th>
-        <th><?php echo _FUNCTIONS; ?></th>
+        <th class="optional">#</span></th>
+        <th class="optional"><?php echo ''._ICON.''; ?></th>
+        <th class="essential persist"><?php echo ''._TITLE.''; ?></th>
+        <th class="optional"><?php echo ''._WEIGHT.''; ?></th>
+        <th class="optional"><?php echo ''._CATALOG_CODE.''; ?></th>
+        <th class="optional"><?php echo ''._DATEINS.''; ?></th>
+        <th class="optional"><?php echo ''._CATEGORY.''; ?></th>
+        <th class="optional"><?php echo ''._LANGUAGE.''; ?></th>
+        <th class="optional"><?php echo ''._STATUS.''; ?></th>
+        <th class="optional"><?php echo ''._HITS.''; ?></th>
+        <th class="optional"><?php echo ''._FUNCTIONS.''; ?></th>
       </tr>
       </thead>
       <tbody>
@@ -45,9 +46,9 @@ function catalog_pagelist() {
   		   $res = $db->sql_query('SELECT *,DATE_FORMAT(date_add, "%d/%m/%Y %H:%i:%s") as date_it 
                                   FROM '.$prefix.'_catalog_products 
                                   ORDER BY weight DESC 
-                                  LIMIT '.$ofsbgn.','.$ofsppg);
+                                  LIMIT '.$ofsbgn.','.$ofsppg.'');
     	   $numrows = $db->sql_numrows($res);
-     while ($row = $db->sql_fetchrow($res,MYSQL_ASSOC)) 
+     while ($row = $db->sql_fetchrow($res)) 
    	       {
 		   $id_catalog = intval($row['id_catalog']);
 		   $id_category = intval($row['id_category']);
@@ -64,31 +65,31 @@ function catalog_pagelist() {
 		   $weight = intval($row['weight']);
 		   $rowcat = $db->sql_fetchrow($db->sql_query('SELECT title,parentid 
 		   											   FROM '.$prefix.'_categories 
-													   WHERE id_category='.$id_category),MYSQL_ASSOC);
+													   WHERE id_category='.$id_category.''));
            $ctitle = stripslashes(check_html($rowcat['title'], "nohtml"));
 		   $rowpat = $db->sql_fetchrow($db->sql_query('SELECT title 
 		   											   FROM '.$prefix.'_partners 
-													   WHERE id_partner='.$id_partner));
-           $partners = stripslashes(check_html($rowpat['title'], "nohtml"),MYSQL_ASSOC);
+													   WHERE id_partner='.$id_partner.''));
+           $partners = stripslashes(check_html($rowpat['title'], "nohtml"));
         if (empty($partners)) { $partners = $aid; }
 		   $rowserv = $db->sql_fetchrow($db->sql_query('SELECT imgfile,attachfile 
 		   											   FROM '.$prefix.'_catalog_service 
-													   WHERE id_catalog='.$id_catalog),MYSQL_ASSOC);
+													   WHERE id_catalog='.$id_catalog.''));
            $imgfile = $rowserv['imgfile'];
            $codehtl = intval($rowserv['attachfile']);
 		   $weight1 = $weight - 1;
 		   $weight3 = $weight + 1;
 		   $row_res = $db->sql_fetchrow($db->sql_query('SELECT id_catalog 
                                                         FROM '.$prefix.'_catalog_products  
-                                                        WHERE weight='.$weight1),MYSQL_ASSOC);
+                                                        WHERE weight='.$weight1.''));
 		   $bid1 = intval($row_res['id_catalog']);
-		   $con1 = $bid1;
+		   $con1 = ''.$bid1.'';
 		   $row_res2 = $db->sql_fetchrow($db->sql_query('SELECT id_catalog 
                                                          FROM '.$prefix.'_catalog_products  
-                                                         WHERE weight='.$weight3),MYSQL_ASSOC);
+                                                         WHERE weight='.$weight3.''));
 		   $bid2 = intval($row_res2['id_catalog']);
-		   $con2 = $bid2;
-		   $oplink = $name_prefix.'_pagelist';
+		   $con2 = ''.$bid2.'';
+		   $oplink = ''.$name_prefix.'_pagelist';
  		   $pag_edit    = '<a href="'.$admin_file.'.php?op='.$name_prefix.'_pageedit&id_catalog='.$id_catalog.'" title="Edit"><i class="splashy-document_letter_edit"></i></a>';
 		   $pag_double  = '<a href="'.$admin_file.'.php?op='.$name_prefix.'_pagedouble&id_catalog='.$id_catalog.'" title="Copy"><i class="splashy-document_copy"></i></a>';
 		   $pag_del  = '<a href="'.$admin_file.'.php?op='.$name_prefix.'_pagedel&id_catalog='.$id_catalog.'" title="Delete"><i class="splashy-document_letter_remove"></i></a>';
@@ -96,18 +97,18 @@ function catalog_pagelist() {
 		   $pag_desc    = '<a href="'.$admin_file.'.php?op='.$name_prefix.'_pagedesc&id_catalog='.$id_catalog.'" title="Description"><i class="splashy-view_list"></i></a>';
 		   $pag_service = '<a href="'.$admin_file.'.php?op='.$name_prefix.'_servlist&id_catalog='.$id_catalog.'" title="Service"><i class="splashy-hcards"></i></a>';
 //		   $pag_gallery = '<a href="'.$admin_file.'.php?op='.$name_prefix.'_gallist&id_catalog='.$id_catalog.'&oplink='.$oplink.'&page='.$page.'">'._IMG_GALLERY.'</a>';
-		   $link_url = 'modules.php?name='.$module_name.'&amp;op=catalog_product&amp;id_catalog='.$id_catalog;
+		   $link_url = 'modules.php?name='.$module_name.'&amp;op=catalog_product&amp;id_catalog='.$id_catalog.'';
 		   ?>
            <tr>
-		     <td><span class="atiny"><?php echo $weight; ?></span></td>
+		     <td><span class="atiny"><?php echo ''.$weight.''; ?></span></td>
 		 	 <td>
 			 <?php
-			 if (empty($codehtl)) { $newimage = $id_catalog.'/'.$imgfile; } else { $newimage = $codehtl.'/'.$imgfile; }
-			 $image_path = 'uploads/images/products/'.$newimage;
+			 if (empty($codehtl)) { $newimage = ''.$id_catalog.'/'.$imgfile.''; } else { $newimage = ''.$codehtl.'/'.$imgfile.''; }
+			 $image_path = 'uploads/images/products/'.$newimage.'';
              $thumb_path = '<img src="'.image_cache('uploads/images/products/'.$newimage,180,135).'" />';		
              $mini_path = '<img src="'.image_cache('uploads/images/products/'.$newimage,50,38).'" />';		
           if ($imgfile != '' && file_exists($image_path)) 
-             { echo '<a href="'.$image_path.'" title="" class="cbox_single thumbnail">'.$mini_path.'</a>'; } 
+             { echo '<a href="'.$image_path.'" title="Image_4" class="cbox_single thumbnail">'.$mini_path.'</a>'; } 
         else { echo '<img src="images/no_images.jpg" border="0" width="30" height="20" />'; }
 			 ?>
              </td>
@@ -120,10 +121,10 @@ function catalog_pagelist() {
 	      if ($con2) { echo '<a href="'.$admin_file.'.php?op='.$name_prefix.'_pagechgorder&weight='.$weight.'&bidori='.$id_catalog.'&weightrep='.$weight3.'&bidrep='.$con2.'&oplink='.$oplink.'&page='.$page.'">'._IMG_BLOCKUP.'</a>'; }
 	         ?>
              </td>
-		     <td><span class="atiny"><?php echo $codepro; ?></span></td>
-		     <td><span class="atiny"><?php echo $mydate; ?></span></td>
-		     <td><span class="atiny"><?php echo $ctitle;?></span></td>
-		     <td><span class="atiny"><?php echo $alanguage; ?></span></td>
+		     <td><span class="atiny"><?php echo ''.$codepro.''; ?></span></td>
+		     <td><span class="atiny"><?php echo ''.$mydate.''; ?></span></td>
+		     <td><span class="atiny"><?php echo ''.$ctitle.'';?></span></td>
+		     <td><span class="atiny"><?php echo ''.$alanguage.''; ?></span></td>
 		     <td class="atiny">
 			 <?php
           if ($mystatus == 0) { echo '<span class="label label-danger sl_status">'._CATALOG_NOACTIVE.'</span>'; }
@@ -134,8 +135,8 @@ function catalog_pagelist() {
 	    else {}
 			 ?>
              </td>
-		 	 <td><span class="atiny"><?php echo $counter; ?></span></td>
-		 	 <td><?php echo $pag_edit.'&nbsp;'.$pag_double.'&nbsp;'.$pag_del.'&nbsp;'.$pag_option.'&nbsp;'.$pag_desc.'&nbsp;'.$pag_service; ?></td>
+		 	 <td><span class="atiny"><?php echo ''.$counter.''; ?></span></td>
+		 	 <td><?php echo ''.$pag_edit.'&nbsp;'.$pag_double.'&nbsp;'.$pag_del.'&nbsp;'.$pag_option.'&nbsp;'.$pag_desc.'&nbsp;'.$pag_service.''; ?></td>
 	 	   </tr> 
 		   <?php
 		   }
